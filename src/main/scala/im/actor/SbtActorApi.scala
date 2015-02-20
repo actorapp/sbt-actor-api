fe82a6bf1e300a13790f349dc339c532a17f3a98
Andrey Kouznetsov <fear@loathing.in>
2015-02-20 05:41:06 +0300

    generate ActorApi in target/src_managed on compile

--- src/main/scala/im/actor/SbtActorApi.scala
+++ src/main/scala/im/actor/SbtActorApi.scala
@@ -1,35 +1,74 @@
 package im.actor
 
 import im.actor.api._
+import java.io.File
 import sbt._, Keys._
 
 object SbtActorApi extends AutoPlugin {
+  val ActorApi = config("actorapi").hide
+
   val path = SettingKey[File]("actor-schema-path", "The path that contains actor.json file")
   val outputPath = SettingKey[File]("actor-schema-output-path", "The paths where to save the generated *.scala files.")
 
-  lazy val generate = TaskKey[Unit]("actor-schema-generate", "Compile json schema to scala code")
+  lazy val actorapi = TaskKey[Seq[File]]("actorapi", "Compile json schema to scala code")
+
+  lazy val actorapiMain = SettingKey[String]("actorapi-main", "ActorApi main class.")
 
   lazy val settings: Seq[Setting[_]] = Seq(
-    path <<= (sourceDirectory in Compile) { _ / "actor-api" },
-    outputPath <<= (sourceDirectory in Compile) { _ / "scala" },
-    generate <<= generateTask
+    sourceDirectory in ActorApi <<= (sourceDirectory in Compile),
+    path <<= sourceDirectory in ActorApi,
+    managedClasspath in ActorApi <<= (classpathTypes, update) map { (ct, report) =>
+      Classpaths.managedJars(ActorApi, ct, report)
+    },
+    outputPath <<= sourceManaged in ActorApi,
+
+    actorapi <<= (
+      sourceDirectory in ActorApi,
+      sourceManaged in ActorApi,
+      managedClasspath in ActorApi,
+      javaHome,
+      streams
+    ).map(generate),
+
+    sourceGenerators in Compile <+= (actorapi).task
   )
 
-  private def generate(srcDir: File, targetDir: File, log: Logger): Unit = {
+  private def generate(srcDir: File, targetDir: File, classpath: Classpath, javaHome: Option[File], streams: TaskStreams): Seq[File] = {
+    val log = streams.log
+
     log.info(f"Generating actor schema for $srcDir%s")
 
-    val schemaFile = srcDir / "actor.json"
-    if (!schemaFile.exists) {
-      log.info(f"Nothing to generate in $srcDir%s")
-    } else {
-      val tree = (new Json2Tree(IO.read(schemaFile))).convert()
-      val targetFile = targetDir / "actor.scala"
-      IO.write(targetFile, tree)
-    }
-  }
+    val input = srcDir / "actor-api"
 
-  private def generateTask: Def.Initialize[Task[Unit]] = (streams, path, outputPath) map {
-    (out, sourceDir, targetDir) =>
-    generate(sourceDir, targetDir, out.log)
+    if (!input.exists) {
+      log.info(f"$input%s does not exists")
+      Nil
+    } else {
+      val output = targetDir / "scala" //"ActorApi.scala"
+
+      println(streams.cacheDirectory / "actor-api")
+
+      val cached = FileFunction.cached(streams.cacheDirectory / "actor-api", FilesInfo.lastModified, FilesInfo.exists) {
+        (in: Set[File]) => {
+          IO.delete(output)
+          IO.createDirectory(output)
+
+          val src = input / "actor.json"
+          if (src.exists()) {
+            val tree = (new Json2Tree(IO.read(src))).convert()
+            val targetFile = output / "ActorApi.scala"
+
+            log.info(f"Generated ActorApi.scala $targetFile%s")
+
+            IO.write(targetFile, tree)
+          } else {
+            log.info(f"no actor.json file in $input%s")
+          }
+
+          (output ** ("*.scala")).get.toSet
+        }
+      }
+      cached((input ** "actor.json").get.toSet).toSeq
+    }
   }
 }
